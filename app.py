9adb0cec263fd639a39ba93302b172972d692f24
hausdinge <hausdinge@gmail.com>
2024-04-15 04:17:07 +0200

    add templating

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ import requests
 MyApp = Flask(__name__)
 
 def get_data():
+	section_num = 5
+	sections = ["Getting started", "Jupyter", "Paraview", "Vistle", "Post-Atom"]
 	urls = [
-     "https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/getting-started.md",
-     "https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/jupyter.md",
-     "https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/paraview.md",
-     "https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/vistle_get_started.md",
-     "https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/firststeps_post_atom.md"
-    ] 
-
+	"https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/getting-started.md",
+	"https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/jupyter.md",
+	"https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/paraview.md",
+	"https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/vistle_get_started.md",
+	"https://raw.githubusercontent.com/bwvisu/bwvisu.github.io/master/user-docs/firststeps_post_atom.md"
+	] 
+ 
 	data = []
 	for i in urls:
 		data.append(requests.get(i).content.decode())
 
-	return data
-
+	return data, section_num, sections
+   
 
 @MyApp.route("/")
 def hello():
-	return render_template('index.html', gitwiki=get_data())
+	dt, sec_n, secs = get_data()
+	return render_template('index.html', gitwiki=dt, section_num = sec_n, sections = secs)
 
 if __name__ == "__main__":
 	MyApp.run()
